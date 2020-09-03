4deaadbbbc117b68445330ddb024e840f01ce880
jasminesandhu46 <jps6818@psu.edu>
2020-09-03 21:30:30 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// Author: Jasmine Sandhu jps6818@psu.edu
+// Collaborator: Brian Truong bqt5199@psu.edu
+// Collaborator: Gabriel Charpentier gbc5202@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *temp = readline("Enter temperature in celsius: ");
+  double cel = atof(temp);
+  double fahr = 1.8*cel + 32;
+printf("%f° in Celsius is equivlent to %f° Fahrenheit.", cel,fahr);
+
+}
